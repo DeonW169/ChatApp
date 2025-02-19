2d715074215a39da983a05bea4a977a0032d50e5
Anroux <anroux@Anrouxs-MacBook-Air.local>
2025-02-19 14:31:57 +0200

    Final ChatApp

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -8,68 +8,67 @@ import { NgModel } from "@angular/forms";
 })
 export class ChatComponent implements OnInit {
   users: any[] = [];
+  messages: any[] = [];
   hasUsers = false;
+  hasChatHistory = false;
   chatMessage = "";
   user = "";
-  message = "";
 
   constructor() {}
 
   ngOnInit() {
-    //1. Get userList from localStorage
-    //2. If userList exists then
-    //  this.usert = JSON.parse(userList)
     let userList = localStorage.getItem("userList");
     if (userList) {
       this.users = JSON.parse(userList);
       this.hasUsers = true;
     }
-  }
 
-  loadChats() {}
+    const defaultUser = this.users[1];
+    this.user = `${defaultUser.name}-${defaultUser.surname}`;
+    this.getChatHistroy();
+  }
 
   sendMessage() {
-    if (this.chatMessage) {
-      const chats = JSON.parse(localStorage.getItem("chats") || "[]");
-      let chat = chats.find(
-        (user: any) =>
-          user.name === this.setSelectedUser &&
-          user.surname === this.setSelectedUser
-      );
-
-      if (!chat) {
-        chat = {
-          name: this.setSelectedUser,
-          surname: this.setSelectedUser,
-          message: [],
-        };
-        chats.push(chat);
-        this.saveChat();
-      }
-    }
-  }
-
-  saveChat() {
-    let chat = {
-      user: this.setSelectedUser,
-      messages: this.message,
-
-      message: {
-        from: "Anroux",
-        to: this.user,
-        date: new Date().toISOString(),
-        message: this.chatMessage,
-      },
+    const message = {
+      from: "Anroux",
+      to: this.user,
+      date: new Date().toISOString(),
+      message: this.chatMessage,
     };
 
-    let chatString = JSON.stringify(chat);
+    let chat = {
+      messages: [message],
+    };
+
+    if (this.hasChatHistory) {
+      this.messages.push(message);
+      chat.messages = this.messages;
+    }
+    this.saveChat(chat);
+  }
+
+  getChatHistroy() {
+    let chat = localStorage.getItem(`${this.user}`);
+    if (chat) {
+      this.messages = JSON.parse(chat).messages;
+      this.hasChatHistory = true;
+      // return true;
+    } else {
+      this.messages = [];
+      this.hasChatHistory = false;
+      // return false;
+    }
+    // return false;
+  }
+
+  saveChat(messages) {
+    let chatString = JSON.stringify(messages);
     localStorage.setItem(`${this.user}`, chatString);
     this.chatMessage = "";
   }
 
   setSelectedUser(user) {
-    debugger;
     this.user = `${user.name}-${user.surname}`;
-    this.loadChats();
+    this.getChatHistroy();
   }
 }
